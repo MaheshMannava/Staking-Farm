880850a9f13fbfda9e651ccdc9f7c369f1e79a33
MaheshMannava <maheshmannava16@gmail.com>
2023-05-23 15:54:43 +0530

    first

---
+++ frontend/components/stakingData.js
@@ -0,0 +1,20 @@
+import styles from "../styles/Home.module.css";
+
+export default function StakingData() {
+  return (
+    <section className={styles.stakingDataContainer}>
+      <section className={styles.stakingData}>
+        <span>Total Staked Tokens</span>
+        <span className={styles.stakingData_value}>-</span>
+      </section>
+      <section className={styles.stakingData}>
+        <span>Total Renewal Paid</span>
+        <span className={styles.stakingData_value}>-</span>
+      </section>
+      <section className={styles.stakingData}>
+        <span>Stakers</span>
+        <span className={styles.stakingData_value}>-</span>
+      </section>
+    </section>
+  );
+}
